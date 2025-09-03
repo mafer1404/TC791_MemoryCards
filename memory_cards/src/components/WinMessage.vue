6301047936b58a6f718a82609a17147153917253
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-09-03 11:38:30 -0600

    Componente para mostrar mensaje de victoria

---
+++ memory_cards/src/components/WinMessage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container my-5 d-flex justify-content-center">
+    <div class="card p-4 text-center w-100">
+      <h2 class="text-success">Congratulations!</h2>
+      <p>You’ve found all the matching pairs in the game!</p>
+      <button class="btn btn-primary mt-3" @click="$emit('restart')">Play again</button>
+    </div>
+  </div>
+</template>
