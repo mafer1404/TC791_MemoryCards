e0aca97e113f33bc292cd4bd6336d3e7c59be007
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-09-03 11:38:20 -0600

    Informacion de las cartas

---
+++ memory_cards/src/data/cardsData.js
@@ -0,0 +1,11 @@
+export const cardsData = [
+  { id: 1, text: "Let’s grow trees.", image: "tree.png", sound: "LetsGrowTrees.mp3", video: "LetsGrowTrees.mp4" },
+  { id: 2, text: "Recycling is good.", image: "recycling.png", sound: "RecyclingIsGood.mp3", video: "RecyclingIsGood.mp4"},
+  { id: 3, text: "I clean my environment.", image: "cleaning.png", sound: "CleanEnvironment.mp3", video: "CleanEnvironment.mp4" },
+  { id: 4, text: "The grass is green and fresh.", image: "grass.png", sound: "GrassGreenFresh.mp3", video: "GrassGreenFresh.mp4" },
+  { id: 5, text: "I take care of my plant.", image: "plant.png", sound: "TakeCarePlant.mp3", video: "CareOfPlant.mp4" },
+  { id: 6, text: "Let’s put the trash in the wastebasket.", image: "trashbin.png", sound: "TrashWastebasket.mp3", video: "TrashInWastebasket.mp4" },
+  { id: 7, text: "I love a clean park.", image: "park.png", sound: "ILoveCleanPark.mp3", video: "CleanPark.mp4" },
+  { id: 8, text: "It’s raining.", image: "rain.png", sound: "ItsRaining.mp3", video: "ItsRaining.mp4" },
+  { id: 9, text: "We reuse paper.", image: "reusedpaper.png", sound: "WeReusePaper.mp3", video: "ReusePaper.mp4" },
+];
